7f5876a233b7732da7f32b6643e031492ddd8f13
Aaron S <32329358+Acarnir@users.noreply.github.com>
2021-04-08 22:03:50 +0100

    index.html and experiments.js uploaded
    Introduction tests for learning JavaScript.

---
+++ experiments.js
@@ -0,0 +1,21 @@
+// learning JS - output for below in console is 500 6000
+
+const monthlyRent = 500;
+const yearlyRent = monthlyRent * 12;
+console.log(yearlyRent);
+
+// learning about Strings
+
+const firstName = "Aaron";
+const lastName = "Swan";
+
+// Old method of using spaces
+// const sentence = "Hello " + firstName + " " + lastName + "! How are you?";
+
+// Revised method of spaces
+
+const sentence = `Hello ${firstName} ${lastName}? How are you?`;
+
+console.log(sentence);
+
+// Booleans
